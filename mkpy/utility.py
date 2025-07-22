05d688ee5f3ae508961497f4e3eefe3010aab405
Santiago León O <santileortiz@gmail.com>
2025-07-22 15:05:41 -0600

    Add scanner API to utility.py

--- mkpy/utility.py
+++ mkpy/utility.py
@@ -1349,6 +1349,59 @@ def pymk_default (skip_snip_cache=[]):
             if t != 'default' and t != 'install' and t != old_t and t not in skip_snip_cache:
                 store ('last_snip', value=t)
 
+##############################
+# File scanner API
+#
+# This is a per character file scanner API for Python. In my experience regex
+# and split/join make some small parsing functions simpler, but for involved
+# tasks, per character traversal is better and I've found Pythin's API to be
+# overly verbose.
+
+class file_scanner:
+    def __init__(self, string):
+        self.string = string
+
+        self.pos = 0
+        self.len = len(self.string)
+        self.is_eof = False
+
+    def advance_char(self):
+        if self.pos < self.len:
+            self.pos += 1
+
+        if self.pos == self.len:
+            self.is_eof = True
+
+    def char(self, c):
+        if self.string[self.pos] == c:
+            self.advance_char()
+            return True
+
+        return False
+
+    def str (self, s):
+        for c in s:
+            if c == self.string[self.pos]:
+                self.advance_char()
+            else:
+                return False
+
+    def to_char (self, c):
+        temp_pos = self.pos
+
+        while not self.is_eof and self.string[self.pos] != c:
+            self.advance_char()
+
+        if self.string[self.pos] == c:
+            self.advance_char()
+            return True
+        else:
+            return False
+
+    def consume_spaces (self):
+        while not self.is_eof and self.string[self.pos].isspace():
+            self.advance_char()
+
 ##########################
 # Custom status logger API
 #
